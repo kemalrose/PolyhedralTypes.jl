b9dc5f33bd6c9e7b8d28d335b4425a1648aa83b3
kemalrose <30701894+kemalrose@users.noreply.github.com>
2024-01-17 08:40:59 +0100

    Added code

--- src/PolyhedralTypes.jl
+++ src/PolyhedralTypes.jl
@@ -1,5 +1,9 @@
 module PolyhedralTypes
 
+export get_delta, get_polyhedral_type, list_all_conic_pols, list_all_polygons
+
+using Oscar, LinearAlgebra, Polymake, Random
+include("functions.jl")
 # Write your package code here.
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,9 @@ using PolyhedralTypes
 using Test
 
 @testset "PolyhedralTypes.jl" begin
-    # Write your tests here.
+	A1 = [0 2 4 2; 2 2 4 6]
+	A2 = [1 2 5 3; 2 2 5 6]
+	Delta = get_delta(A1, A2)
+	psi = get_polyhedral_type(A1,A2)
+    @test psi == (20, 19, 0, 4, 3, 0, 526, 10, 6, 0, 6, 0)
 end
