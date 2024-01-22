c5a33e3409c467fd614bfc72fe97e50b9f7e07f1
kemalrose <30701894+kemalrose@users.noreply.github.com>
2024-01-22 11:53:14 +0100

    Also export the command "is_conic"

--- src/PolyhedralTypes.jl
+++ src/PolyhedralTypes.jl
@@ -1,6 +1,6 @@
 module PolyhedralTypes
 
-export get_delta, get_polyhedral_type, list_all_conic_pols, list_all_polygons
+export get_delta, get_polyhedral_type, list_all_conic_pols, list_all_polygons, is_conic
 
 using Oscar, LinearAlgebra, Polymake, Random
 include("functions.jl")
